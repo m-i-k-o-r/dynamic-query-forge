77f1a0cc0e3a99edffe8e9378830093f6df200c8
Королицкий Михаил <koroli.m.v@gmail.com>
2025-01-08 17:43:27 +0300

    init

---
+++ src/main/java/com/koroli/dynamicqueryforge/DynamicQueryForgeApplication.java
@@ -0,0 +1,13 @@
+package com.koroli.dynamicqueryforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQueryForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicQueryForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koroli/dynamicqueryforge/DynamicQueryForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koroli.dynamicqueryforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQueryForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
